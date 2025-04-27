c717c5d46ed4f75a3f1d7f6fe9140c9a13351189
Aziz <azizrasiodv99@gmail.com>
2025-04-28 00:20:33 +0500

    part1 Final

--- Part1/Main.java
+++ Part1/Main.java
@@ -1,4 +1,29 @@
 package Part1;
 
 public class Main {
+    public static void main(String[] args) {
+        Season season1 = new Season();
+        season1.addEpisode(new Episode("Pilot", 1800));
+        season1.addEpisode(new Episode("Episode 2", 1750));
+        season1.addEpisode(new Episode("Episode 3", 1900));
+
+        System.out.println("Обычный порядок");
+        IEpisodeIterator normalIt = new SeasonIterator(season1.getEpisodes());
+        while (normalIt.hasNext()) {
+            System.out.println(normalIt.next());
+        }
+
+        System.out.println("Обратный порядок ");
+        IEpisodeIterator reverseIt = new ReverseSeasonIterator(season1.getEpisodes());
+        while (reverseIt.hasNext()) {
+            System.out.println(reverseIt.next());
+        }
+
+        System.out.println("Случайный порядок ");
+        IEpisodeIterator shuffleIt = new ShuffleSeasonIterator(season1.getEpisodes(), 42);
+        while (shuffleIt.hasNext()) {
+            System.out.println(shuffleIt.next());
+        }
+    }
 }
+
--- Part1/ShuffleSeasonIterator.java
+++ Part1/ShuffleSeasonIterator.java
@@ -1,4 +1,26 @@
 package Part1;
 
-public class ShuffleSeasonIterator {
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+import java.util.Random;
+
+// Итератор по эпизодам в случайном порядке с фиксированным сидом
+public class ShuffleSeasonIterator implements IEpisodeIterator {
+    private List<Episode> shuffled;
+    private int index = 0;
+
+    public ShuffleSeasonIterator(List<Episode> episodes, long seed) {
+        // Копируем список, чтобы не менять оригинал
+        shuffled = new ArrayList<>(episodes);
+        Collections.shuffle(shuffled, new Random(seed)); // фиксируем порядок
+    }
+
+    public boolean hasNext() {
+        return index < shuffled.size();
+    }
+
+    public Episode next() {
+        return shuffled.get(index++);
+    }
 }
